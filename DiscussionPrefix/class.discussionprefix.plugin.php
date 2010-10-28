d9016cc738039f89207f1047cc1488fdd5d327fe
lincolnwebs <lincolnwebs@gmail.com>
2010-10-28 17:53:20 -0400

    DiscussionPrefix: permission fix

--- DiscussionPrefix/class.discussionprefix.plugin.php
+++ DiscussionPrefix/class.discussionprefix.plugin.php
@@ -5,12 +5,12 @@
 $PluginInfo['DiscussionPrefix'] = array(
    'Name' => 'DiscussionPrefix',
    'Description' => 'Allows you to mark certain discussions with a prefix.',
-   'Version' => '1.0a',
+   'Version' => '1.1',
    'RegisterPermissions' => array(
-      'Plugins.DiscussionPrefix.Prefix.Use',
-      'Plugins.DiscussionPrefix.Prefix.Manage'),
+      'Vanilla.DiscussionPrefix.Use',
+      'Vanilla.DiscussionPrefix.Manage'),
    'SettingsUrl' => '/plugin/discussionprefix',
-   'SettingsPermission' => 'Plugins.DiscussionPrefix.Prefix.Manage',
+   'SettingsPermission' => 'Vanilla.DiscussionPrefix.Manage',
    'Author' => "Matt Lincoln Russell",
    'AuthorEmail' => 'lincolnwebs@gmail.com',
    'AuthorUrl' => 'http://www.tmprod.com/web-development/vanilla.php',
@@ -24,14 +24,14 @@ class DiscussionPrefixPlugin extends Gdn_Plugin {
     */
    public function Base_GetAppSettingsMenuItems_Handler(&$Sender) {
       $Menu = &$Sender->EventArguments['SideMenu'];
-      $Menu->AddLink('Forum', 'Discussion Prefix', 'plugin/discussionprefix', 'Plugins.DiscussionPrefix.Prefix.Manage');
+      $Menu->AddLink('Forum', 'Discussion Prefix', 'plugin/discussionprefix', 'Vanilla.DiscussionPrefix.Manage');
    }
    
    /**
     * Creates a virtual DiscussionPrefix controller
     */
    public function PluginController_DiscussionPrefix_Create(&$Sender) {
-      $Sender->Permission('Plugins.DiscussionPrefix.Prefix.Manage');
+      $Sender->Permission('Vanilla.DiscussionPrefix.Manage');
       $Sender->Title('Discussion Prefix');
       $Sender->Form = new Gdn_Form();
       $this->Dispatch($Sender, $Sender->RequestArgs);
@@ -42,7 +42,7 @@ class DiscussionPrefixPlugin extends Gdn_Plugin {
     */
    public function PostController_DiscussionFormOptions_Handler(&$Sender) {
       $Session = Gdn::Session();
-      if ($Session->CheckPermission('Plugins.DiscussionPrefix.Prefix.Use'))
+      if ($Session->CheckPermission('Vanilla.DiscussionPrefix.Use'))
          $Sender->EventArguments['Options'] .= '<li>'.$Sender->Form->CheckBox('Prefixed', C('Plugins.DiscussionPrefix.Label'), array('value' => '1')).'</li>';
    }
    
